1e9ade37a83789a6e36345de75e0ddbadcf62052
Brock Whittaker <brock@zulipchat.com>
2017-11-16 16:09:24 -0800

    user-groups: Add basic design, capability.

--- static/js/settings_user_groups.js
+++ static/js/settings_user_groups.js
@@ -12,6 +12,29 @@ exports.reset = function () {
 
 exports.set_up = function () {
     meta.loaded = true;
+
+    var group_map = {
+        // user ids
+        Backend: [32, 33, 34, 35, 36],
+        Frontend: [37, 33, 38, 39],
+        API: [34, 35, 40],
+    };
+
+    $("[data-group-pills]").each(function () {
+        var $this = $(this);
+        var pills = input_pill($this);
+        var group_name = $this.data("group-pills");
+
+        if (Array.isArray(group_map[group_name])) {
+            group_map[group_name].forEach(function (user_id) {
+                var user = people.get_person_from_user_id(user_id);
+
+                if (user) {
+                    pills.pill.append(user.full_name, user_id);
+                }
+            });
+        }
+    });
 };
 
 return exports;
